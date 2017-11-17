b543e9a0d758136e845bf677191c35ca80d0f5db
Andrei Gavrila <kiuny@github.com>
2017-11-18 00:23:22 +0200

    intial laravel server + migrations and models

---
+++ app/Http/Controllers/TrafficControllerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\TrafficController;
+use Illuminate\Http\Request;
+
+class TrafficControllerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\TrafficController  $trafficController
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TrafficController $trafficController)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\TrafficController  $trafficController
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TrafficController $trafficController)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\TrafficController  $trafficController
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TrafficController $trafficController)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\TrafficController  $trafficController
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TrafficController $trafficController)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/TrafficLightController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\TrafficLight;
+use Illuminate\Http\Request;
+
+class TrafficLightController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\TrafficLight  $trafficLight
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TrafficLight $trafficLight)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\TrafficLight  $trafficLight
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TrafficLight $trafficLight)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\TrafficLight  $trafficLight
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TrafficLight $trafficLight)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\TrafficLight  $trafficLight
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TrafficLight $trafficLight)
+    {
+        //
+    }
+}
